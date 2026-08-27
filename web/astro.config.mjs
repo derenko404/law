de104a93b684aea91408de465572f5c912314fcf
Vasyl Derenko <vasylderenko@MacBook-Air-Vasyl.local>
2026-08-27 09:50:43 +0200

    fixes

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -10,6 +10,10 @@ export default defineConfig({
   site: 'https://advokat-uzhgorod.com.ua',
   adapter: cloudflare(),
 
+  build: {
+    inlineStylesheets: 'always',
+  },
+
   vite: {
     plugins: [tailwindcss()],
   },
